6b235ea91ca8d537efe82499356f9d475b7a95f9
Jiří Vrba <jiri@vrba.dev>
2022-12-10 13:47:44 +0100

    Application is now somehow working

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,38 @@
 import { useContext } from "react";
-import { AppContext } from "../App";
+import { ApplicationContext } from "../layout/ApplicationLayout";
 
 export interface HeaderProps {
-    title: string,
-    questionsCount: number,
-    children: React.ReactNode,
+    title: string;
+    questionsCount: number;
 }
 
-const Header: React.FC<HeaderProps> = ({ title, questionsCount, children }: HeaderProps) => {
-    const { darkmode } = useContext(AppContext);
+const Header: React.FC<HeaderProps> = ({
+    title,
+    questionsCount,
+}: HeaderProps) => {
+    const { darkmode } = useContext(ApplicationContext);
 
     return (
-        <header className={`flex flex-col lg:flex-row p-3 lg:p-10 ${darkmode ? 'bg-black' : 'bg-gray-200'} items-start`}>
-            <div className="flex flex-col items-center lg:items-start w-full lg:w-auto">
-                <h1 className={`text-center lg:text-left text-lg lg:text-3xl font-bold ${darkmode ? 'text-neutral-200' : 'text-black'}`}>{title}</h1>
-                <p className="text-sm text-neutral-500 uppercase font-bold tracking-widest lg:mt-3">Tester obsahuje {questionsCount} otázek</p>
-            </div>
-
-            <div className="mt-3 lg:mt-0 flex flex-grow flex-row items-center justify-center lg:justify-start w-full lg:w-auto">
-                {children}
+        <header
+            className={`flex flex-col p-3 lg:flex-row lg:p-10 ${
+                darkmode ? "bg-black" : "bg-gray-200"
+            } items-start`}
+        >
+            <div className="flex w-full flex-col items-center lg:w-auto lg:items-start">
+                <h1
+                    className={`text-center text-lg font-bold lg:text-left lg:text-3xl ${
+                        darkmode ? "text-neutral-200" : "text-black"
+                    }`}
+                >
+                    {title}
+                </h1>
+                <p className="text-sm font-bold uppercase tracking-widest text-neutral-500 lg:mt-3">
+                    Tester obsahuje {questionsCount} otázek
+                </p>
+                <div className=""></div>
             </div>
         </header>
-    )
-}
-
+    );
+};
 
 export default Header;
