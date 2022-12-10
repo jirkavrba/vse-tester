a9e3457d51634936b24ec81c3daa5d6ed9383962
Jiří Vrba <jiri@vrba.dev>
2022-12-10 13:56:03 +0100

    Fix AppContext

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,33 @@
 import { useContext } from "react";
 import { FaGithub } from "react-icons/fa";
-import { AppContext } from "../App";
+import { ApplicationContext } from "../layout/ApplicationLayout";
 
 const Footer: React.FC = () => {
-    const {darkmode} = useContext(AppContext);
+    const { darkmode } = useContext(ApplicationContext);
 
     return (
-        <footer className={`${darkmode ? "bg-black" : "bg-gray-100"} flex flex-row justify-between py-5 px-10`}>
-            <span className={`${darkmode ? "text-neutral-700" : "text-neutral-400"} uppercase text-xs font-black`}>VŠE Tester</span>
-            <a className="text-gray-500" href="https://github.com/jirkavrba/vse-tester" target="_blank" rel="noreferrer">
+        <footer
+            className={`${
+                darkmode ? "bg-black" : "bg-gray-100"
+            } flex flex-row justify-between py-5 px-10`}
+        >
+            <span
+                className={`${
+                    darkmode ? "text-neutral-700" : "text-neutral-400"
+                } text-xs font-black uppercase`}
+            >
+                VŠE Tester
+            </span>
+            <a
+                className="text-gray-500"
+                href="https://github.com/jirkavrba/vse-tester"
+                target="_blank"
+                rel="noreferrer"
+            >
                 <FaGithub />
             </a>
         </footer>
     );
-}
+};
 
 export default Footer;
