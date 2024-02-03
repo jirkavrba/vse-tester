f26e38f7be2511366ffdcf390cc89ecf92f6150e
Jiří Vrba <jiri@vrba.dev>
2024-02-03 17:12:18 +0100

    Zbroják pro Michala <3

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -1,7 +1,7 @@
 import type { QuestionSet } from "../types";
 
 export const fetchQuestionSets = async (): Promise<Array<QuestionSet>> => {
-    const sets = ["1fp201", "3sg201", "4it115", "4sa310", "4st204", "11f201", "4iz210"];
+    const sets = ["1fp201", "3sg201", "4it115", "4sa310", "4st204", "11f201", "4iz210", "zbrojni-prukaz"];
     const promises = sets.map(async (set: string) => {
         const source = await import(`../questions/${set}.json`);
 
